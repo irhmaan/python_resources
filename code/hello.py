4c1b1acfbdab93a221b83080d16ec751ee095563
irhmaan <devirehmaan@gmail.com>
2026-07-17 09:39:54 +0530

    refact: repo structure changed and following google notes for learning.

---
+++ code/hello.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    print("hi, there", sys.argv[1])
+    #* sys.argv contains the args passed , 0 is for prg name & can be ignored.
+
+
+#* standard code to call main() function to begin
+
+if __name__ == "__main__":
+    main()
